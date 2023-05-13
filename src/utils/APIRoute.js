bd82642a11cbebfe4a8b18cd6315f9b90e3ceda8
Nam <realcm471@gmail.com>
2023-05-13 17:12:08 +0700

    .

--- src/utils/APIRoute.js
+++ src/utils/APIRoute.js
@@ -1,5 +1,4 @@
 // export const baseURL = "https://gobadelivery-v1.onrender.com";
 export const baseURL = process.env.REACT_APP_BASE_URL;
 
-export const MAPBOX_API_KEY =
-  "pk.eyJ1IjoicGhhbm5nb2NuYW0iLCJhIjoiY2xkdWcwbTBnMDVoajN6cXRzeW5qMzR3MyJ9.tNB2ynexTO48NP1wu5Jp4Q";
+export const MAPBOX_API_KEY = process.env.REACT_APP_MAPBOX_API_KEY;
