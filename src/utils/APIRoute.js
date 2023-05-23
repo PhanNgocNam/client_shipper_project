bf1cd3a13966dda2c5e3bffa9283168bbc9fddc6
Nam <realcm471@gmail.com>
2023-05-23 14:38:23 +0700

    fix UI bug

--- src/utils/APIRoute.js
+++ src/utils/APIRoute.js
@@ -1,6 +1,6 @@
-// export const baseURL = "http://localhost:4940";
+export const baseURL = "http://localhost:4940";
 // export const baseURL = process.env.REACT_APP_BASE_URL;
 // export const MAPBOX_API_KEY = process.env.REACT_APP_MAPBOX_API_KEY;
 
-export const baseURL = process.env.REACT_APP_BASE_URL;
+// export const baseURL = process.env.REACT_APP_BASE_URL;
 export const MAPBOX_API_KEY = process.env.REACT_APP_MAPBOX_API_KEY;
